16bbf8af3043eccac509681d28eb24b052e2358e
Arnelle Balane <arnellebalane@gmail.com>
2017-01-31 17:48:01 +0800

    url-parser lab exercise boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ *  Parses the given URL into its different components.
+ *
+ *  TODO: Implement this function.
+ *  NOTE: You may implement additional functions as you need, as long as this
+ *    function behaves as specified in the instructions. Have fun! :)
+ **/
+function parse(url) {
+
+}
